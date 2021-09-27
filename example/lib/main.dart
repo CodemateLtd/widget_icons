a98b728a9564a6427decf6b0099fcd02c2d60344
Tom Gilder <tom.gilder@codemate.com>
2021-09-27 13:17:41 +0300

    Update icons

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,59 +13,174 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: ListView(
         children: const [
-          IconContainer(icon: WidgetIcons.alert_dialog, name: 'alert_dialog'),
-          IconContainer(icon: WidgetIcons.align, name: 'align'),
-          IconContainer(icon: WidgetIcons.animated, name: 'animated'),
-          IconContainer(icon: WidgetIcons.app_bar, name: 'app_bar'),
-          IconContainer(icon: WidgetIcons.bottom_bar, name: 'bottom_bar'),
-          IconContainer(icon: WidgetIcons.card, name: 'card'),
-          IconContainer(icon: WidgetIcons.center, name: 'center'),
-          IconContainer(icon: WidgetIcons.checkbox, name: 'checkbox'),
-          IconContainer(icon: WidgetIcons.circle_avatar, name: 'circle_avatar'),
+          IconContainer(
+            icon: WidgetIcons.alert_dialog,
+            name: 'alert_dialog',
+          ),
+          IconContainer(
+            icon: WidgetIcons.align,
+            name: 'align',
+          ),
+          IconContainer(
+            icon: WidgetIcons.animated,
+            name: 'animated',
+          ),
+          IconContainer(
+            icon: WidgetIcons.app_bar,
+            name: 'app_bar',
+          ),
+          IconContainer(
+            icon: WidgetIcons.bottom_bar,
+            name: 'bottom_bar',
+          ),
+          IconContainer(
+            icon: WidgetIcons.card,
+            name: 'card',
+          ),
+          IconContainer(
+            icon: WidgetIcons.center,
+            name: 'center',
+          ),
+          IconContainer(
+            icon: WidgetIcons.checkbox,
+            name: 'checkbox',
+          ),
+          IconContainer(
+            icon: WidgetIcons.circle_avatar,
+            name: 'circle_avatar',
+          ),
           IconContainer(
             icon: WidgetIcons.circular_progress,
             name: 'circular_progress',
           ),
-          IconContainer(icon: WidgetIcons.column, name: 'column'),
+          IconContainer(
+            icon: WidgetIcons.column,
+            name: 'column',
+          ),
           IconContainer(
             icon: WidgetIcons.constrained_box,
             name: 'constrained_box',
           ),
-          IconContainer(icon: WidgetIcons.container, name: 'container'),
-          IconContainer(icon: WidgetIcons.divider, name: 'divider'),
-          IconContainer(icon: WidgetIcons.drawer, name: 'drawer'),
-          IconContainer(icon: WidgetIcons.expand, name: 'expand'),
+          IconContainer(
+            icon: WidgetIcons.container,
+            name: 'container',
+          ),
+          IconContainer(
+            icon: WidgetIcons.divider,
+            name: 'divider',
+          ),
+          IconContainer(
+            icon: WidgetIcons.drawer,
+            name: 'drawer',
+          ),
+          IconContainer(
+            icon: WidgetIcons.flexible,
+            name: 'flexible',
+          ),
           IconContainer(
             icon: WidgetIcons.floating_action_button,
             name: 'floating_action_button',
           ),
-          IconContainer(icon: WidgetIcons.gesture, name: 'gesture'),
-          IconContainer(icon: WidgetIcons.gridview, name: 'gridview'),
-          IconContainer(icon: WidgetIcons.hero, name: 'hero'),
-          IconContainer(icon: WidgetIcons.icon, name: 'icon'),
-          IconContainer(icon: WidgetIcons.image, name: 'image'),
-          IconContainer(icon: WidgetIcons.inkwell, name: 'inkwell'),
-          IconContainer(icon: WidgetIcons.listview, name: 'listview'),
-          IconContainer(icon: WidgetIcons.material, name: 'material'),
-          IconContainer(icon: WidgetIcons.opacity, name: 'opacity'),
           IconContainer(
-            icon: WidgetIcons.outlinedbutton,
-            name: 'outlinedbutton',
+            icon: WidgetIcons.gesture,
+            name: 'gesture',
           ),
-          IconContainer(icon: WidgetIcons.padding, name: 'padding'),
-          IconContainer(icon: WidgetIcons.radio, name: 'radio'),
-          IconContainer(icon: WidgetIcons.root, name: 'root'),
-          IconContainer(icon: WidgetIcons.row, name: 'row'),
-          IconContainer(icon: WidgetIcons.scaffold, name: 'scaffold'),
-          IconContainer(icon: WidgetIcons.scroll, name: 'scroll'),
-          IconContainer(icon: WidgetIcons.sizedbox, name: 'sizedbox'),
-          IconContainer(icon: WidgetIcons.stack, name: 'stack'),
-          IconContainer(icon: WidgetIcons.tab, name: 'tab'),
-          IconContainer(icon: WidgetIcons.text, name: 'text'),
-          IconContainer(icon: WidgetIcons.text_button, name: 'text_button'),
-          IconContainer(icon: WidgetIcons.toggle, name: 'toggle'),
-          IconContainer(icon: WidgetIcons.transition, name: 'transition'),
-          IconContainer(icon: WidgetIcons.wrap, name: 'wrap'),
+          IconContainer(
+            icon: WidgetIcons.grid_view,
+            name: 'grid_view',
+          ),
+          IconContainer(
+            icon: WidgetIcons.hero,
+            name: 'hero',
+          ),
+          IconContainer(
+            icon: WidgetIcons.icon,
+            name: 'icon',
+          ),
+          IconContainer(
+            icon: WidgetIcons.image,
+            name: 'image',
+          ),
+          IconContainer(
+            icon: WidgetIcons.inkwell,
+            name: 'inkwell',
+          ),
+          IconContainer(
+            icon: WidgetIcons.list_view,
+            name: 'list_view',
+          ),
+          IconContainer(
+            icon: WidgetIcons.material,
+            name: 'material',
+          ),
+          IconContainer(
+            icon: WidgetIcons.opacity,
+            name: 'opacity',
+          ),
+          IconContainer(
+            icon: WidgetIcons.outlined_button,
+            name: 'outlined_button',
+          ),
+          IconContainer(
+            icon: WidgetIcons.padding,
+            name: 'padding',
+          ),
+          IconContainer(
+            icon: WidgetIcons.page_view,
+            name: 'page_view',
+          ),
+          IconContainer(
+            icon: WidgetIcons.radio_button,
+            name: 'radio_button',
+          ),
+          IconContainer(
+            icon: WidgetIcons.root,
+            name: 'root',
+          ),
+          IconContainer(
+            icon: WidgetIcons.row,
+            name: 'row',
+          ),
+          IconContainer(
+            icon: WidgetIcons.scaffold,
+            name: 'scaffold',
+          ),
+          IconContainer(
+            icon: WidgetIcons.scroll,
+            name: 'scroll',
+          ),
+          IconContainer(
+            icon: WidgetIcons.sized_box,
+            name: 'sized_box',
+          ),
+          IconContainer(
+            icon: WidgetIcons.stack,
+            name: 'stack',
+          ),
+          IconContainer(
+            icon: WidgetIcons.tab,
+            name: 'tab',
+          ),
+          IconContainer(
+            icon: WidgetIcons.text,
+            name: 'text',
+          ),
+          IconContainer(
+            icon: WidgetIcons.text_button,
+            name: 'text_button',
+          ),
+          IconContainer(
+            icon: WidgetIcons.toggle,
+            name: 'toggle',
+          ),
+          IconContainer(
+            icon: WidgetIcons.transition,
+            name: 'transition',
+          ),
+          IconContainer(
+            icon: WidgetIcons.wrap,
+            name: 'wrap',
+          ),
         ],
       ),
     );
---
+++ test/widget_theme_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:widget_icons/widget_icons.dart';
+import 'package:widget_icons/widget_theme.dart';
+
+void main() {
+  test('Correct asset from widget with a type', () {
+    const String widgetName = 'AnimatedBuilder<String>';
+    final theme = WidgetTheme.fromName(widgetName);
+    expect(theme.icon, WidgetIcons.animated);
+  });
+
+  test('Has default theme for custom widget', () {
+    const String widgetName = 'CustomWidget';
+    final theme = WidgetTheme.fromName(widgetName);
+    expect(theme.color, WidgetTheme.otherWidgetColor);
+  });
+}
